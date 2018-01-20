5a00c2d7e8660ae16f944e1f71d85f882bd2f681
swaminathan.42@osu.edu <swaminathan.42@osu.edu>
2018-01-20 16:59:13 -0500

    Added login activity, uses ssh to stdlinux box to verify the user credentials

---
+++ app/src/main/java/printah/osu/printah/LoginActivity.kt
@@ -0,0 +1,180 @@
+package printah.osu.printah
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.annotation.TargetApi
+import android.content.Context
+import android.os.AsyncTask
+import android.os.Build
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import android.widget.Toast
+import com.jcraft.jsch.JSch
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    private val preferencesFile = "osu.cse.printah.PREFERENCE_FILE_KEY"
+    private var mAuthTask: UserLoginTask? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        password.setOnEditorActionListener(TextView.OnEditorActionListener { _, id, _ ->
+            if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+                attemptLogin()
+                return@OnEditorActionListener true
+            }
+            false
+        })
+        email_sign_in_button.setOnClickListener { attemptLogin() }
+        attemptLoginWithSavedCredentials()
+    }
+
+    private fun attemptLoginWithSavedCredentials() {
+        val sharedPreferences = getSharedPreferences(preferencesFile, Context.MODE_PRIVATE)
+        val username = sharedPreferences.getString("username", "")
+        val password = sharedPreferences.getString("password", "")
+        if (username != "" && password != "") {
+            login(username, password)
+        }
+    }
+
+    private fun attemptLogin() {
+        if (mAuthTask != null) {
+            return
+        }
+
+        email.error = null
+        password.error = null
+
+        val emailStr = email.text.toString()
+        val passwordStr = password.text.toString()
+
+        var cancel = false
+        var focusView: View? = null
+
+        if (!TextUtils.isEmpty(passwordStr) && !isPasswordValid(passwordStr)) {
+            password.error = getString(R.string.error_invalid_password)
+            focusView = password
+            cancel = true
+        }
+
+        if (!isEmailValid(emailStr)) {
+            email.error = getString(R.string.error_invalid_email)
+            focusView = email
+            cancel = true
+        }
+
+        if (cancel) {
+            focusView?.requestFocus()
+        } else {
+            login(emailStr, passwordStr)
+        }
+    }
+
+    private fun login(emailStr: String, passwordStr: String) {
+        showProgress(true)
+        mAuthTask = UserLoginTask(emailStr, passwordStr)
+        mAuthTask!!.execute(null as Void?)
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        val osuUserNameRegex = Regex(pattern = "[a-zA-Z]+\\.\\d+")
+        return osuUserNameRegex.matches(email)
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+        return password.length > 1
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private fun showProgress(show: Boolean) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            val shortAnimTime = resources.getInteger(android.R.integer.config_shortAnimTime).toLong()
+
+            login_form.visibility = if (show) View.GONE else View.VISIBLE
+            login_form.animate()
+                    .setDuration(shortAnimTime)
+                    .alpha((if (show) 0 else 1).toFloat())
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator) {
+                            login_form.visibility = if (show) View.GONE else View.VISIBLE
+                        }
+                    })
+
+            login_progress.visibility = if (show) View.VISIBLE else View.GONE
+            login_progress.animate()
+                    .setDuration(shortAnimTime)
+                    .alpha((if (show) 1 else 0).toFloat())
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator) {
+                            login_progress.visibility = if (show) View.VISIBLE else View.GONE
+                        }
+                    })
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            login_progress.visibility = if (show) View.VISIBLE else View.GONE
+            login_form.visibility = if (show) View.GONE else View.VISIBLE
+        }
+    }
+
+    private fun onLoginSuccess(username: String, mPassword: String) {
+        Toast.makeText(applicationContext, "Login successful", Toast.LENGTH_SHORT).show()
+        val sharedPref = getSharedPreferences(preferencesFile, Context.MODE_PRIVATE)
+        with(sharedPref.edit()) {
+            putString("username", username)
+            putString("password", mPassword)
+            commit()
+        }
+    }
+
+    inner class UserLoginTask internal constructor(private val username: String, private val mPassword: String) : AsyncTask<Void, Void, Boolean>() {
+
+        override fun doInBackground(vararg params: Void): Boolean? {
+            val config = java.util.Properties()
+            config.put("StrictHostKeyChecking", "no")
+            val jSch = JSch()
+            val host = "stdlinux.cse.ohio-state.edu"
+            val session = jSch.getSession(username, host)
+            session.setPassword(mPassword)
+            session.setConfig(config)
+            return try {
+                session.connect()
+                Log.i(localClassName, "Login successful")
+                val connected = session.isConnected
+                session.disconnect()
+                connected
+            } catch (e: Exception) {
+                Log.e("Unable to login", e.message, e)
+                false
+            }
+        }
+
+        override fun onPostExecute(success: Boolean?) {
+            mAuthTask = null
+            showProgress(false)
+
+            if (success!!) {
+                onLoginSuccess(username, mPassword)
+            } else {
+                password.error = getString(R.string.error_incorrect_password)
+                password.requestFocus()
+            }
+        }
+
+        override fun onCancelled() {
+            mAuthTask = null
+            showProgress(false)
+        }
+    }
+
+}
